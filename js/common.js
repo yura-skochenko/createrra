2aecfc52c286a2a7b930e2091bf46ac3aa9bb199
yura-skochenko <skochenkoy@gmail.com>
2015-03-31 01:33:06 +0300

    page 404,cart,catalouge, cabinet

---
+++ js/common.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+                /* amount product +/- */
+                $('.minus').click(function () {
+                    var $input = $(this).parents('.number').find('input');
+                    var count = parseInt($input.val()) - 1;
+                    count = count < 1 ? 1 : count;
+                    $input.val(count);
+                    $input.change();
+                    return false;
+
+                });
+                $('.plus').click(function () {
+                    var $input = $(this).parents('.number').find('input');
+                    $input.val(parseInt($input.val()) + 1);
+                    $input.change();
+                    return false;
+                });
+                /* select */
+                $(function () {
+                    $(".select_product").selectbox();
+                    $(".select-light").selectbox();
+                });
+                /* tabs */
+                (function($) {
+                    $(function() {
+                        $('ul.tabs').delegate('li:not(.current)', 'click', function() {
+                            $(this).addClass('current').siblings().removeClass('current')
+                                    .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(150);
+                        })
+                    })
+                })(jQuery);
+                /* carousel */
+                $('.carousel').bxSlider({
+                    slideWidth: 210,
+                    minSlides: 2,
+                    maxSlides: 4,
+                    moveSlides: 1,
+                    pager: false,
+                    slideMargin: 0
+                });
+            });
